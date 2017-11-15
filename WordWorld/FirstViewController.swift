187a792503aedfb0c0458848c098e76830a1f10f
haiyongsong1921 <sunhy339@126.com>
2017-11-16 06:24:24 +0800

    first commit but failed on iphone debug

--- WordWorld/FirstViewController.swift
+++ WordWorld/FirstViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
+import SQLite
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    var wordsDict = [(String, Word)]()
+
+    let name_links_tuples : [(String,String)] =
+        [
+            ("四川麻将iphone","https://itunes.apple.com/cn/app/bo-ya-si-chuan-ma-jiang/id510582247?mt=8"),
+            ("四川麻将ipad","https://itunes.apple.com/cn/app/bo-ya-si-chuan-ma-jianghd/id551932207?mt=8"),
+            ("二人麻将iphone","https://itunes.apple.com/cn/app/bo-ya-er-ren-ma-jiang/id687598024?l=en&mt=8"),
+            ("二人麻将ipad","https://itunes.apple.com/cn/app/bo-ya-er-ren-ma-jianghd/id768019415?l=en&mt=8"),
+        ]
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       // return wordsDict.count
+        return name_links_tuples.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: nil)
+
+ //       let title :String = wordsDict[indexPath.row].0
+        let title :String = name_links_tuples[indexPath.row].0
+        cell.textLabel!.text = title
+        cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        do {
+            let db = try Connection("/Users/sunhaiyang/Documents/GitRepo/iosTest/WordWorld/WordWorld/words.db")
+            let word = Word()
+            let wordList = Table("CET4WORDSTEST1")
+            let id = Expression<Int64>("Id")
+            let english = Expression<String>("English")
+            let chinese = Expression<String>("Chinese")
+            let level = Expression<Int64>("Level")
+
+            for wordRow in try db.prepare(wordList) {
+//                print("english: \(wordRow[english]), chinese: \(wordRow[chinese]), level: \(wordRow[level])")
+            word.id = wordRow[id]
+            word.engWord = wordRow[english]
+            word.chineseWord = wordRow[chinese]
+            word.level = wordRow[level]
+            wordsDict.append((word.engWord, word))
+                print("english: \(word.engWord), chinese: \(word.chineseWord), level: \(word.level)")
+            }
+            tableView.dataSource = self
+     //       tableView.delegate = self
+     //       self.view.addSubview(tableView)
+//            self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ WordWorld/Words/Words.swift
@@ -0,0 +1,15 @@
+//
+//  Words.swift
+//  WordWorld
+//
+//  Created by 孙海洋 on 12/11/2017.
+//  Copyright © 2017 Adam Ltd. All rights reserved.
+//
+
+import Foundation
+class Word {
+    var id: Int64 = 0
+    var engWord: String!
+    var chineseWord: String!
+    var level: Int64 = 0
+}
